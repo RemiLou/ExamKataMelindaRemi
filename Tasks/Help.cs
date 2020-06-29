6132b0bc7a5d8388aba1438718fb4cceed5c6f55
Yaourt404 <remi.loubiou@gmail.com>
2020-06-29 10:26:29 +0200

    Add Help class

---
+++ Tasks/Help.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks
+{
+    public class Help : Command
+    {
+        public override void CommandInput()
+        {
+            console.WriteLine("Commands:");
+            console.WriteLine("  show");
+            console.WriteLine("  add project <project name>");
+            console.WriteLine("  add task <project name> <task description>");
+            console.WriteLine("  check <task ID>");
+            console.WriteLine("  uncheck <task ID>");
+            console.WriteLine();
+        }
+    }
+}
