780329e798afbb99749cfdbd8887648239f51a5a
Yaourt404 <remi.loubiou@gmail.com>
2020-06-29 09:59:19 +0200

    Add Error, Add Uncheck

---
+++ Tasks/Error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks
+{
+    public class Error : Command
+    {
+        public override void CommandType()
+        {
+            console.WriteLine("I don't know what the command \"{0}\" is.", command);
+        }
+    }
+}
