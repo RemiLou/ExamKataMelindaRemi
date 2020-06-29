c602f39b879687eddab7366dd1de0ba83a33a450
Yaourt404 <remi.loubiou@gmail.com>
2020-06-29 12:39:28 +0200

    Fix Add

--- Tasks/CommandFactory.cs
+++ Tasks/CommandFactory.cs
@@ -16,7 +16,7 @@ namespace Tasks
 					commandType = new Show();
 					break;
 				case "add":
-					commandType = new Add(commandText[0], commandText[1], commandText[1].Split(" ".ToCharArray(), 2)[1]); // Param Type + Name + Description
+					commandType = new Add(commandText.Split(" ".ToCharArray(), 2)[1], commandText[2], commandText[1].Split(" ".ToCharArray(), 2)[1]); // Param Type + Name + Description
 					break;
 				case "check":
 					commandType = new Check(commandText[1]); // Param
