0138b4dede1ed1710b0fb1205d30f85753b94240
Yaourt404 <remi.loubiou@gmail.com>
2020-06-29 09:49:38 +0200

    Add Show Class

--- Tasks/ICommand.cs
+++ Tasks/ICommand.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Text;
 
 namespace Tasks
 {
     public interface ICommand
     {
-
+        void Command();
     }
 }
---
+++ Tasks/Show.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks
+{
+    public class Show : ICommand
+    {
+        public void Command()
+        {
+			foreach (var project in tasks)
+			{
+				console.WriteLine(project.Key);
+				foreach (var task in project.Value)
+				{
+					console.WriteLine("    [{0}] {1}: {2}", (task.Done ? 'x' : ' '), task.Id, task.Description);
+				}
+				console.WriteLine();
+			}
+		}
+    }
+}
